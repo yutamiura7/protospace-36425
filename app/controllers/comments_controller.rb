adcbfde39fdc35f972d01f04937775bd9936f910
yutamiura7 <yuta6112miura@icloud.com>
2021-08-09 10:31:00 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    if comment = Comment.create(comment_params)
+      redirect_to "/prototypes/#{comment.prototype.id}"
+    else
+      render :show
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
